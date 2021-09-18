ac527f379f6e122b2044ada432357b25b4bafcac
Frank Cichos <f.cichos@me.com>
2021-09-18 13:02:08 +0200

    update value

--- source/.ipynb_checkpoints/conf-checkpoint.py
+++ source/.ipynb_checkpoints/conf-checkpoint.py
@@ -23,7 +23,7 @@ author = 'Frank Cichos'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1'
+release = '21'
 
 
 # -- General configuration ---------------------------------------------------
